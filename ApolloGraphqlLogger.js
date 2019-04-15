92d9856ee21c55e038606df61baaeffbf0ad8792
Talal Arshad <talal@kudosnow.com>
2019-04-15 14:12:03 -0600

    replaces line breaks

--- ApolloGraphqlLogger.js
+++ ApolloGraphqlLogger.js
@@ -5,13 +5,13 @@ module.exports = (logger) => {
     // eslint-disable-next-line class-methods-use-this
     requestDidStart({ queryString, parsedQuery, variables }) {
       const query = queryString || print(parsedQuery);
-      logger.info(query);
+      logger.info(query.replace(/(\r\n|\n|\r)/gm, ' '));
       logger.info(variables);
     }
 
     // eslint-disable-next-line class-methods-use-this
     willSendResponse({ graphqlResponse }) {
-      logger.debug(JSON.stringify(graphqlResponse, null, 2));
+      logger.debug(JSON.stringify(graphqlResponse, null, 2).replace(/(\r\n|\n|\r)/gm, ' '));
     }
   }
   return BasicLogging;
