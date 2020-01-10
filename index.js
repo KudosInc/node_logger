f68f4a0a427a25582137d3dea9f8361a979a05bf
Talal Arshad <talal@kudosnow.com>
2020-01-10 12:17:57 -0700

    adds option for extra info in info debug and warning

--- index.js
+++ index.js
@@ -152,18 +152,18 @@ class Logger {
     this.response = {};
   }
 
-  info(message) {
-    this.build({ message, severity: LEVELS.info });
+  info(message, extraInfo = {}) {
+    this.build({ message, severity: LEVELS.info, ...extraInfo });
     this.output();
   }
 
-  debug(message) {
-    this.build({ message, severity: LEVELS.debug });
+  debug(message, extraInfo = {}) {
+    this.build({ message, severity: LEVELS.debug, ...extraInfo });
     this.output();
   }
 
-  warn(message) {
-    this.build({ message, severity: LEVELS.warning });
+  warn(message, extraInfo) {
+    this.build({ message, severity: LEVELS.warning, ...extraInfo });
     this.output();
   }
 
