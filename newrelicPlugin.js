a1ee81ff860158a88ab3366c1d44bf4e8add950e
Colin Mackenzie <colin.mackenzie@kudos.com>
2020-08-20 10:24:08 -0600

    KD-1759-reduce-new-relic-noise

--- newrelicPlugin.js
+++ newrelicPlugin.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-unresolved */
 const newrelic = require('newrelic');
-const helper = require('./helper');
 const { getOr } = require('lodash/fp');
+const helper = require('./helper');
 
-const EXCLUDE_ERROR_FROM_NEW_RELIC = new RegExp(getOr(null,process.env.IGNORED_ERRORS));
+const EXCLUDE_ERROR_FROM_NEW_RELIC = new RegExp(getOr(null, process.env.IGNORED_ERRORS));
 
 module.exports = class NewRelicPlugin {
   // eslint-disable-next-line class-methods-use-this
@@ -27,10 +27,9 @@ module.exports = class NewRelicPlugin {
 
   // eslint-disable-next-line class-methods-use-this
   didEncounterErrors(rc) {
-    if(rc[0]['message'].match(EXCLUDE_ERROR_FROM_NEW_RELIC)) {
+    if (rc[0].message.match(EXCLUDE_ERROR_FROM_NEW_RELIC)) {
       return;
     }
-      
     newrelic.noticeError(rc[0]);
   }
 };
