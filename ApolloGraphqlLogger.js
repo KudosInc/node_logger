11993509149924b5f20d313af5558a5b8f58772d
Jessie Klein <jessie@kudosnow.com>
2021-09-01 15:34:49 -0600

    test

--- ApolloGraphqlLogger.js
+++ ApolloGraphqlLogger.js
@@ -5,7 +5,7 @@ module.exports = (logger) => {
     // eslint-disable-next-line class-methods-use-this
     requestDidStart({ request: { query, variables, operationName } }) {
       const { query: parsedQuery } = helper.parseGraphQLQuery(String(query), operationName);
-      console.log('query', query);
+      console.log('parsedQuery', parsedQuery);
       console.log('variables', variables);
       logger.graphqlRequest({
         parsedQuery,
