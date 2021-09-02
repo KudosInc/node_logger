ae1f05410f6603d220acdaade7320f68afb81abe
Jessie Klein <jessie@kudosnow.com>
2021-09-02 07:10:25 -0600

    KD-3159 Fetch operation name.

--- ApolloGraphqlLogger.js
+++ ApolloGraphqlLogger.js
@@ -2,38 +2,21 @@ const helper = require('./helper');
 
 module.exports = (logger) => {
   const BasicLogging = {
-    // eslint-disable-next-line class-methods-use-this
-    requestDidStart(requestContext) {
-      console.log('REQUEST CONTEXT', requestContext);
+    requestDidStart({ request: { query, variables } }) {
+      const {
+        query: parsedQuery, action, gqlVerb,
+      } = helper.parseGraphQLQuery(query, variables);
+      logger.graphqlRequest({
+        query: parsedQuery,
+        variables,
+        action,
+        gqlVerb,
+      });
       return {
-        didResolveOperation(r) {
-          console.log('!!!!!', r);
-          return {
-            // eslint-disable-next-line class-methods-use-this
-            willSendResponse({ response }) {
-              logger.graphqlResponse(response);
-            },
-          };
+        willSendResponse({ response }) {
+          logger.graphqlResponse(response);
         },
       };
-      // console.log('!!!!!', request);
-      // const { query, variables, operationName } = request;
-      // const {
-      //   query: parsedQuery, action, gqlVerb,
-      // } = helper.parseGraphQLQuery(query, operationName);
-      // console.log(getDetailsFromDocument(requestContext));
-      // logger.graphqlRequest({
-      //   query: parsedQuery,
-      //   variables,
-      //   action,
-      //   gqlVerb,
-      // });
-      // return {
-      //   // eslint-disable-next-line class-methods-use-this
-      //   willSendResponse({ response }) {
-      //     logger.graphqlResponse(response);
-      //   },
-      // };
     },
   };
   return BasicLogging;
--- helper.js
+++ helper.js
@@ -1,14 +1,16 @@
-const { first } = require('lodash/fp');
+const { first, isEmpty } = require('lodash/fp');
 
 const extraSpacesNewLineRemovalRegexp = new RegExp(/(\r\n|\n|\r|\s\s+)/gm);
 const QUERY_MUTATION_PATTERN = new RegExp(/query|mutation/);
-const QUERY_ACTION_PATTERN = new RegExp(/^\s*\w+\s+(\w+)/);
 
+const operationName = (queryString, variables) => {
+  if (isEmpty(variables)) return queryString.split('+').pop().split('{')[0];
+  return queryString.split('+').pop().split('(')[0];
+};
 
-const parseGraphQLQuery = (queryString, operationName) => {
+const parseGraphQLQuery = (queryString, variables) => {
+  const action = operationName(queryString, variables);
   const query = queryString ? queryString.replace(extraSpacesNewLineRemovalRegexp, ' ') : '';
-  const match = QUERY_ACTION_PATTERN.exec(query);
-  const action = operationName || match ? match[1] : 'no action';
   const gqlVerb = first(query.match(QUERY_MUTATION_PATTERN));
 
   return { query, action, gqlVerb };
