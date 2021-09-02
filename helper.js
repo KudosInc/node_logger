cf366a93c065dace24ee8cbf796f2dfcdef51d60
Jessie Klein <jessie@kudosnow.com>
2021-09-02 07:48:44 -0600

    ..

--- helper.js
+++ helper.js
@@ -1,13 +1,17 @@
-const { first, isEmpty } = require('lodash/fp');
+const { first, isEmpty, trim } = require('lodash/fp');
 
 const extraSpacesNewLineRemovalRegexp = new RegExp(/(\r\n|\n|\r|\s\s+)/gm);
 const QUERY_MUTATION_PATTERN = new RegExp(/query|mutation/);
 
 const operationName = (queryString, variables) => {
+  let action;
   if (isEmpty(variables)) {
-    return queryString.split('{')[1].replace(extraSpacesNewLineRemovalRegexp, ' ');
+    // eslint-disable-next-line prefer-destructuring
+    action = queryString.split('{')[1];
+  } else {
+    [action] = queryString.split('{')[1].split('(');
   }
-  return queryString.split('{')[1].split('(')[0].replace(extraSpacesNewLineRemovalRegexp, ' ');
+  return trim(action.replace(extraSpacesNewLineRemovalRegexp, ' '));
 };
 
 const parseGraphQLQuery = (queryString, variables) => {
