9f52f7f60c7654cfce6b97dfffc367d485f8702b
Jessie Klein <jessie@kudosnow.com>
2021-09-02 07:40:06 -0600

    KD-3159 Operation name.

--- helper.js
+++ helper.js
@@ -4,15 +4,14 @@ const extraSpacesNewLineRemovalRegexp = new RegExp(/(\r\n|\n|\r|\s\s+)/gm);
 const QUERY_MUTATION_PATTERN = new RegExp(/query|mutation/);
 
 const operationName = (queryString, variables) => {
-  console.log('queryString', queryString);
-  console.log('isEmpty(variables)', isEmpty(variables));
-  if (isEmpty(variables)) return queryString.split('{')[1];
-  return queryString.split('{')[1].split('(')[0];
+  if (isEmpty(variables)) {
+    return queryString.split('{')[1].replace(extraSpacesNewLineRemovalRegexp, ' ');
+  }
+  return queryString.split('{')[1].split('(')[0].replace(extraSpacesNewLineRemovalRegexp, ' ');
 };
 
 const parseGraphQLQuery = (queryString, variables) => {
   const action = operationName(queryString, variables);
-  console.log('action', action);
   const query = queryString ? queryString.replace(extraSpacesNewLineRemovalRegexp, ' ') : '';
   const gqlVerb = first(query.match(QUERY_MUTATION_PATTERN));
 
